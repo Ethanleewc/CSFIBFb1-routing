5035c5b824ae5533d916826672cd725861305691
kenken64 <bunnyppl@hotmail.com>
2023-03-27 13:25:15 +0800

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ElephantComponent } from './components/elephant.component';
+import { HomeComponent } from './components/home.component';
+import { TrexComponent } from './components/trex.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent },
+  { path:'elephant', component: ElephantComponent },
+  { path:'trex', component: TrexComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csf-routing';
+
+  constructor(private router:Router){
+
+  }
+
+  gotoElephant(){
+    console.log("going !");
+    this.router.navigate(['/elephant']);
+
+  }
 }
